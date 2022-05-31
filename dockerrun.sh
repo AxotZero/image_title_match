ce7a3447d29e92752de5acc035eab45e7b1c153b
AxotZero <41388159+AxotZero@users.noreply.github.com>
2022-05-31 10:58:28 +0800

    Delete dockerrun.sh

--- dockerrun.sh
+++
@@ -1,8 +0,0 @@
-docker run \
-        --volume=/media/hd03/axot_data/image_title_match_submit/submit:/workspace/image_title_match \
-        --volume=/media/md01/home/axot/img_text_data/data:/workspace/image_title_match/contest_data \
-        --shm-size=10g \
-        --gpus=all \
-        --name=submit \
-        -it img_text:v1 \
-        bash
